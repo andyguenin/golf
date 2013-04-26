2cce9b4adb77ca236cba04a9feac1afd1e17f3ab
Andy Guenin <andyguenin@gmail.com>
2013-04-26 19:15:52 -0400

    continue

--- Gemfile
+++ Gemfile
@@ -7,7 +7,9 @@ gem 'rails', '3.2.3'
 
 gem 'pg'
 
-
+group :development do
+  gem 'seed_dump'
+end
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_url, :notice => "Logged in!"
     else
-      flash.now.error = "Invalid email or password"
+      flash.now[:error] = "Invalid email or password"
       render "new"
     end
   end
