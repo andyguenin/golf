a49e138cac9910a4336abf497ea36c77b96585bd
Andy Guenin <andyguenin@gmail.com>
2014-03-16 18:10:54 -0400

    prod changes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,10 @@ server '162.243.252.118', user: 'deploy', roles: %w{web app db}, primary: true
 
 set :ssh_options_to, "/home/deploy/prod/golf"
 
+set :deploy_to, "/home/deploy/prod/golf"
+
+set :use_sudo, true
+
 set :ssh_options, { :forward_agent => true }
 
 set :rails_env, :production
